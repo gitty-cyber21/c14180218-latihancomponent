859c542973d617be727db76410c52e1deb79459b
gitty-cyber21 <c14180218@john.petra.ac.id>
2021-03-07 21:06:38 +0700

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, Page2Component, Page3Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
